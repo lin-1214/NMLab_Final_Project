e2cf1f82ea5f1e2e73a520acea7c1367617452ec
lin-1214 <b10901059@g.ntu.edu.tw>
2023-12-10 14:45:37 +0800

    add RPi server TODO

---
+++ RPi_controller/RPi_server.py
@@ -0,0 +1,29 @@
+import asyncio
+import json
+import os
+import websocket
+import subprocess
+
+# Add modules here in case u need
+# TODO: Build websocket connect to web service
+
+
+
+
+# TODO: Encrypt the information accepted from web
+test_info = {"username": "nmlab", "password": "0000", "signature": "", "verifier": ""}
+
+
+
+# TODO: Generate signature and verifier by tpm, openssl, and add into test_info
+signature = ""
+verifier = ""
+
+
+
+test_info["signature"] = signature
+test_info["verifier"] = verifier
+
+
+# TODO: Send back user info
+
