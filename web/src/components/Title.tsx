c7cbda7df24fd2575f16546a0776641fefc020d6
lin-1214 <b10901059@g.ntu.edu.tw>
2023-12-22 03:28:02 +0800

    loginpage frontend done

--- web/src/components/Title.tsx
+++ web/src/components/Title.tsx
@@ -2,20 +2,17 @@ import "./styles/Title.scss";
 import { FC } from "react";
 
 interface TitleProps {
-    name?: string;
-    children?: React.ReactNode;
+  name?: string;
+  children?: React.ReactNode;
 }
 
 const Title: FC<TitleProps> = ({ children, name }) => {
-    return (
-        <div className="Title">
-            <h1>
-                {name ? ` ${name}'s` : "My"}
-                {" Chat Room"}
-            </h1>
-            {children}
-        </div>
-    );
+  return (
+    <div className="Title">
+      <h1>Stellar Secrete Conclave✨</h1>
+      {children}
+    </div>
+  );
 };
 
 export default Title;
