12f72edef8d8969358d169cdd0b87d2690e8de4f
YCK1130 <ken1130yang@gmail.com>
2023-12-22 00:21:44 +0800

    add TODO

--- web/src/hooks/useChat.ts
+++ web/src/hooks/useChat.ts
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { messageTypes, sendDataTypes } from "../types";
 console.log("i enter useChat");
-var client = new WebSocket("ws://localhost:4000");
+const backendURL = "localhost:4000";
+const RPiURL = "localhost:5000";
+var client = new WebSocket(`ws://${backendURL}`);
+var RPiClient = new WebSocket(`ws://${RPiURL}`);
 console.log(client.readyState);
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
@@ -20,27 +23,58 @@ const useChat = () => {
     const [status, setStatus] = useState<{ type?: string; msg?: string }>({});
     const [msgSent, setMsgSent] = useState(false);
 
-    const sendData = async (data: [sendDataTypes, messageTypes | "init"]) => {
-        if (client.readyState === client.CONNECTING) {
-            let msg = "Server Connnecting. Please try again later!";
+    const sendData = async (data: [sendDataTypes, messageTypes | "init"], attempt = 0) => {
+        if (attempt > 5) {
+            let msg = "RPi Connnection Failed. Please try again later!";
             setStatus({ type: "error", msg: msg });
+            return;
+        }
+        if (client.readyState === client.CONNECTING) {
+            sendData(data, attempt + 1);
             return;
         }
         if (client.readyState === client.CLOSED || client.readyState === client.CLOSING) {
             setStatus({ type: "info", msg: "Connecting..." });
             await delay(500);
-            client = new WebSocket("ws://localhost:4000");
+            client = new WebSocket(`ws://${backendURL}`);
             console.log(client.readyState);
             if (client.readyState === client.CONNECTING) await delay(2000);
             if (client.readyState !== client.OPEN) {
-                let msg = "Connnection Failed. Please try again later!";
-                console.log(msg);
-                setStatus({ type: "error", msg: msg });
+                sendData(data, attempt + 1);
                 return;
             } else {
                 setStatus({ type: "success", msg: "Connected!" });
                 askInit();
                 client.onmessage = (byteString) => clientOnMsg(byteString);
+            }
+        }
+        client.send(JSON.stringify(data));
+    };
+
+    // TODO: change sendData to sendCryptoData if send to rpi
+    const sendCryptoData = async (data: [sendDataTypes, messageTypes], attempt = 0) => {
+        let client = RPiClient;
+        if (attempt > 5) {
+            let msg = "Connnection Failed. Please try again later!";
+            setStatus({ type: "error", msg: msg });
+            return;
+        }
+        if (client.readyState === client.CONNECTING) {
+            sendCryptoData(data, attempt + 1);
+            return;
+        }
+        if (client.readyState === client.CLOSED || client.readyState === client.CLOSING) {
+            setStatus({ type: "info", msg: "Connecting..." });
+            await delay(500);
+            RPiClient = client = new WebSocket(`ws://${RPiURL}`);
+            console.log(client.readyState);
+            if (client.readyState === client.CONNECTING) await delay(2000);
+            if (client.readyState !== client.OPEN) {
+                sendCryptoData(data, attempt + 1);
+                return;
+            } else {
+                setStatus({ type: "success", msg: "Connected!" });
+                RPiClient.onmessage = (byteString) => RPiOnMsg(byteString);
             }
         }
         client.send(JSON.stringify(data));
@@ -95,21 +129,15 @@ const useChat = () => {
                 break;
         }
     };
+    // TODO: add RPiOnMsg to handle RPiClient
+    const RPiOnMsg = (byteString: MessageEvent<string>) => {
+        const { data } = byteString;
+        console.log(data);
+    };
     client.onclose = (e) => {
-        console.log("Socket is closed. Reconnect will be attempted in 5 second.", e.reason);
-        console.log(client.readyState);
+        console.log("Socket is closed.", e.reason);
         let msg = "Server Lost. Reconnect will be attempted in 5 second!";
         setStatus({ type: "fatal error", msg: msg });
-        setTimeout(async () => {
-            console.log("trying");
-            client = new WebSocket("ws://localhost:4000");
-            await delay(500);
-            if (client.readyState === client.OPEN) {
-                setStatus({ type: "success", msg: "Connected!" });
-                client.onmessage = (byteString) => clientOnMsg(byteString);
-                askInit();
-            }
-        }, 5000);
     };
     client.onmessage = (byteString) => clientOnMsg(byteString);
     return {
